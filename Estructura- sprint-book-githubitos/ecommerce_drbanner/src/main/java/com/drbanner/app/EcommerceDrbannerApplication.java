fe26102e709c02a74bde733bd29253d80b162f24
emperatrizm <emperatriz.668@gmail.com>
2022-08-16 11:47:36 -0500

    estructura-sprint

---
+++ Estructura- sprint-book-githubitos/ecommerce_drbanner/src/main/java/com/drbanner/app/EcommerceDrbannerApplication.java
@@ -0,0 +1,13 @@
+package com.drbanner.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDrbannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDrbannerApplication.class, args);
+	}
+
+}
---
+++ Estructura- sprint-book-githubitos/ecommerce_drbanner/src/test/java/com/drbanner/app/EcommerceDrbannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drbanner.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDrbannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
