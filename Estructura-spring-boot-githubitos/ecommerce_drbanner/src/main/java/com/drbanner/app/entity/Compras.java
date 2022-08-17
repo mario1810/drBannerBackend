05c2f3146ae7015b3913ea8fed9b43dcc7a8e480
Aarón Fernández <107515757+AaronFR27@users.noreply.github.com>
2022-08-16 22:58:54 -0500

    Agrego: tabla compras

--- Estructura-spring-boot-githubitos/ecommerce_drbanner/src/main/java/com/drbanner/app/entity/Compras.java
+++ Estructura-spring-boot-githubitos/ecommerce_drbanner/src/main/java/com/drbanner/app/entity/Compras.java
@@ -2,6 +2,7 @@ package com.drbanner.app.entity;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.sql.Time;
 
 import javax.persistence.*;
 
@@ -14,7 +15,18 @@ import lombok.Data;
 @Table(name="compras")
 public class Compras implements Serializable{
 	
-
+private static final long serialVersionUID = 1L;
 	
-
+	//Llave primaria
+	@Id//primary key
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idCompra;
+	@ManyToOne(fetch=FetchType.LAZY) //Foreign key
+	@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+	private Usuarios idUsuario;
+	private Date fechaCompra;
+	private Time horaCompra;
+	private Boolean comprado;
+	private double costoTotal;
+	
 }
