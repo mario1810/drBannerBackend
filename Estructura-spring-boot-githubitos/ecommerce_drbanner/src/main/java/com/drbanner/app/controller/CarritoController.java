345a618575bffa4a5c1d968df278fd9b921b872b
Mario Alvarez <mario.alvarez1810@gmail.com>
2022-08-19 12:06:42 -0500

    Se añade controller de Carrito

--- Estructura-spring-boot-githubitos/ecommerce_drbanner/src/main/java/com/drbanner/app/controller/CarritoController.java
+++ Estructura-spring-boot-githubitos/ecommerce_drbanner/src/main/java/com/drbanner/app/controller/CarritoController.java
@@ -43,7 +43,32 @@ public class CarritoController {
 		List<PedidosDTO> carritoFinal= new ArrayList<PedidosDTO>();
 		List<Pedidos> aux=pedidosService.findAllPedidos();
 		for(int i=0; i<aux.size();i++) {
+			
 			if(aux.get(i).getCompras().getIdCompra()==id && aux.get(i).getCompras().getCarrito()==0) {
+				
+				PedidosDTO aux2= new PedidosDTO();
+				aux2.setDirección(aux.get(i).getDireccion());
+				aux2.setFecha(aux.get(i).getFecha());
+				aux2.setIdPedido(aux.get(i).getIdPedido());
+				aux2.setNombrePaquete(aux.get(i).getPaquetes().getNombrePaquete());
+				aux2.setNombreCategoria(aux.get(i).getPaquetes().getCategorias().getNombreCategoria());
+				aux2.setCosto(aux.get(i).getPaquetes().getCosto());
+				carritoFinal.add(aux2);
+			}
+		}
+		return carritoFinal;
+	}
+	
+	
+	/*Se ejecuta este request cuando se abre la página de pago*/
+	@GetMapping("/carrito/historial/{id}")
+	public List<PedidosDTO> getCarritoHistorial(@PathVariable Long id) {
+
+		List<PedidosDTO> carritoFinal= new ArrayList<PedidosDTO>();
+		List<Pedidos> aux=pedidosService.findAllPedidos();
+		for(int i=0; i<aux.size();i++) {
+			
+			if(aux.get(i).getCompras().getUsuarios().getIdUsuario()==id) {
 				
 				PedidosDTO aux2= new PedidosDTO();
 				aux2.setDirección(aux.get(i).getDireccion());
